844c5a1e0a8fb86247e7e05171852e77bbe451d6
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-02-15 11:44:24 -0600

    Testing

--- StringLoops.java
+++ StringLoops.java
@@ -1,10 +1,72 @@
-public class Loops {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLoops {
 
   // static methods go here
+
+  //works\/
+  public static String reverseCharacters(String str){
+    String revtext = "";
+    char letter;
+    for(int num=0;num<str.length();num++){
+      letter=str.charAt(num);
+      revtext= letter +revtext;
+    }
+    // 
+    return revtext;
+  }
+
+  //works \/
+  public static void indexesOfAll(String str,char seaching){
+    List<Integer> list = new ArrayList<Integer>();
+    for (int a =0;a<str.length();a++){
+      if(str.charAt(a)==seaching){
+        list.add(a+1);
+      }
+    }
+    System.out.println(list);
+  }
+
+  // works \/
+  public static boolean hasRepeatedConsecutives(String str){
+    boolean tf = false;
+    
+    for (int a =0;a<str.length();a++){
+      if(a+1<str.length()){
+        if(str.charAt(a)==str.charAt(a+1)){
+          tf = true;
+        }
+      }
+    }
+    return tf;
+  }
+//works \/
+  public static void reverseWords(String str){
+    String revtext = "";
+    String[] word= str.split(" ");
+    for(String ss : word){
+      
+      revtext= ss+" " +revtext;
+    }
+    System.out.println(revtext);
+  }
+
+
 
   public static void main(String[] args) {
 
     // testing of static methods goes here
-
+    System.out.println(reverseCharacters("null"));
+    indexesOfAll("null plus skill issue numnnnnber",'n');
+    System.out.println(hasRepeatedConsecutives("try hard"));
+    reverseWords("Something longer than pie.");
+   
+    
   }
 }
+
+//CpeSee
+
+
+//javac StringLoops.java; java StringLoops
